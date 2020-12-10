e10e9216ce00ba824292eb62f5cfc4ad32d6b099
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-10 17:22:54 +0900

    feature: add dummy login form

--- survey_frontend/pages/index.tsx
+++ survey_frontend/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { TextField } from "@material-ui/core";
 
-const RootPage : React.FC = () => {
+const RootPage: React.FC = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div>Login</div>
+      <div>
+        <div>email</div>
+        <TextField type="email" />
+        <div>password</div>
+        <TextField type="password" />
+      </div>
+    </div>
   );
-}
+};
 
 export default RootPage;
