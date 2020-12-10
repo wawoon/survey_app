aa4c1f9c9d025711c036d176eefaaab0e5c3ed8f
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-10 18:00:20 +0900

    feature: add final-form

--- survey_frontend/pages/index.tsx
+++ survey_frontend/pages/index.tsx
@@ -1,17 +1,48 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
 import { TextField } from "@material-ui/core";
 
 const RootPage: React.FC = () => {
   return (
-    <div>
-      <div>Login</div>
-      <div>
-        <div>email</div>
-        <TextField type="email" />
-        <div>password</div>
-        <TextField type="password" />
-      </div>
-    </div>
+    <Form
+      onSubmit={(data) => {
+        console.log(data);
+      }}
+      render={({ handleSubmit }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <div>Login</div>
+              <div>
+                <div>email</div>
+                <Field name="email">
+                  {(props) => (
+                    <TextField
+                      type="email"
+                      name={props.input.name}
+                      value={props.input.value}
+                      onChange={props.input.onChange}
+                    />
+                  )}
+                </Field>
+                <div>password</div>
+                <Field name="password">
+                  {(props) => (
+                    <TextField
+                      type="password"
+                      name={props.input.name}
+                      value={props.input.value}
+                      onChange={props.input.onChange}
+                    />
+                  )}
+                </Field>
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        );
+      }}
+    ></Form>
   );
 };
 
