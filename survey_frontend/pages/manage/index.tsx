c1257dacd4425499b2439e7bb2e7a3dd8db79c83
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-12 02:53:01 +0900

    feature: show surveys list

--- survey_frontend/pages/manage/index.tsx
+++ survey_frontend/pages/manage/index.tsx
@@ -1,5 +1,28 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import store from "../../store";
+
 const ManageIndex = () => {
-  return <div>ManageIndex</div>;
+  const [surveys, setSurveys] = useState<any[]>([]);
+  useEffect(() => {
+    const f = async () => {
+      const res = await Axios.get("http://localhost:3000/v1/surveys", {
+        headers: {
+          Authorization: `Bearer ${store.getState().auth.accessToken}`,
+        },
+      });
+      setSurveys(res.data.surveys);
+      console.log(res);
+    };
+    f();
+  }, []);
+
+  return (
+    <div>
+      <div>ManageIndex</div>
+      <code>{JSON.stringify(surveys, null, 2)}</code>
+    </div>
+  );
 };
 
 export default ManageIndex;
