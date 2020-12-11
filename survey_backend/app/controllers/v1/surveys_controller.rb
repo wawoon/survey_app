835fee2ff338e170f3e1f00db7a715b9433f0006
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-12 03:15:10 +0900

    feature: add surveys api

--- survey_backend/app/controllers/v1/surveys_controller.rb
+++ survey_backend/app/controllers/v1/surveys_controller.rb
@@ -3,4 +3,37 @@ class V1::SurveysController < ApplicationController
     @surveys = Survey.all
     render json: { surveys: @surveys.as_json }
   end
+
+  def create
+    @survey = current_user.surveys.build(survey_params[:survey])
+    if @survey.save
+      render json: { survey: @survey.as_json }
+    else
+      render json: { survey: @survey.as_json, errors: @survey.errors }
+    end
+  end
+
+  def update
+    @survey = Survey.find(params[:survey][:id])
+    if @survey.update(survey_params[:survey])
+      render json: { survey: @survey.as_json }
+    else
+      render json: { survey: @survey.as_json, errors: @survey.errors }
+    end
+  end
+
+  def destroy
+    @survey = Survey.find(params[:survey][:id])
+    if @survey.destroy
+      render json: { message: "success" }
+    else
+      render json: { message: "failed to delete the survey" }
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(%w[title content])
+  end
 end
