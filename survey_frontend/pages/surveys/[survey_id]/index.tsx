4697652a59d4a3513c0c1612e1357688d3c12215
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-15 14:53:39 +0900

    feature: show survey result

--- survey_frontend/pages/surveys/[survey_id]/index.tsx
+++ survey_frontend/pages/surveys/[survey_id]/index.tsx
@@ -36,11 +36,30 @@ const useStyles = makeStyles({
   },
 });
 
+type ResponseType = {
+  id: number;
+  user_email: string;
+  user_name: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type ResponseCreateAPIResponse = {
+  response: ResponseType;
+  respondent_uuid: string;
+};
+
+type DetailSurveyResponse = {
+  survey: DetailSurvey;
+  has_submitted: boolean;
+};
+
 type DetailSurvey = {
   id: number;
   user_id: number;
   title: string;
   content?: string;
+  response_count: number;
   created_at: string;
   updated_at: string;
   questions: {
@@ -54,6 +73,7 @@ type DetailSurvey = {
       id: number;
       question_id: number;
       name: string;
+      answer_count: number;
       created_at: string;
       updated_at: string;
     }[];
@@ -94,7 +114,7 @@ const ResponseForm: React.FC<{ survey: DetailSurvey }> = (props) => {
         choice_ids: data.choice_ids.map((id) => parseInt(id)),
       };
 
-      const ret = await Axios.post(
+      const ret = await Axios.post<ResponseCreateAPIResponse>(
         `http://localhost:3000/api/v1/surveys/${props.survey.id}/responses`,
         {
           response: dataToSend,
@@ -135,7 +155,6 @@ const ResponseForm: React.FC<{ survey: DetailSurvey }> = (props) => {
         submitting,
         values,
       }) => {
-        console.log(values);
         return (
           <form onSubmit={handleSubmit}>
             <Card>
@@ -215,9 +234,54 @@ const ResponseForm: React.FC<{ survey: DetailSurvey }> = (props) => {
   );
 };
 
+const SurveyResultQustionCard: React.FC<{
+  question: DetailSurvey["questions"][0];
+}> = (props) => {
+  const classes = useStyles();
+  const question = props.question;
+  return (
+    <Card style={{ margin: "12px 0" }}>
+      <CardContent>
+        <Typography className={classes.header}>{question.name}</Typography>
+        {question.choices.map((choice, i) => {
+          return (
+            <div>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography>
+                  {i + 1}. {choice.name}
+                </Typography>
+                <Typography>
+                  {choice.answer_count} votes{" "}
+                  {question.answer_count != 0 &&
+                    `(${choice.answer_count / question.answer_count})`}
+                </Typography>
+              </div>
+            </div>
+          );
+        })}
+      </CardContent>
+    </Card>
+  );
+};
+
+const SurveyResult: React.FC<{ survey: DetailSurvey }> = (props) => {
+  const survey = props.survey;
+  return (
+    <div>
+      <Typography>{survey.response_count} people has submitted.</Typography>
+      {survey.questions.map((q) => (
+        <SurveyResultQustionCard key={q.id} question={q} />
+      ))}
+    </div>
+  );
+};
+
 const SurveyShow = () => {
   const router = useRouter();
   const [survey, setSurvey] = useState<DetailSurvey | null>(null);
+  const [hasSubmittedResponse, setHasSubmittedResponse] = useState<boolean>(
+    false
+  );
   const classes = useStyles();
   const [respondentUuid] = useRespondentUuid();
 
@@ -225,7 +289,7 @@ const SurveyShow = () => {
     if (!router.query.survey_id) return;
 
     const f = async () => {
-      const res = await Axios.get<{ survey: DetailSurvey }>(
+      const res = await Axios.get<DetailSurveyResponse>(
         `http://localhost:3000/api/v1/surveys/${router.query.survey_id}`,
         {
           headers: {
@@ -235,27 +299,48 @@ const SurveyShow = () => {
         }
       );
       setSurvey(res.data.survey);
+      setHasSubmittedResponse(res.data.has_submitted);
       console.log(res);
     };
     f();
   }, [router.query.survey_id]);
   const loading = !survey;
 
+  if (loading) {
+    return (
+      <div>
+        <Header />
+        <Container maxWidth={"md"}>
+          <Loading />
+        </Container>
+      </div>
+    );
+  }
+
+  const componentAfterSubmition = (
+    <Box>
+      <Typography className={classes.header}>{survey.title}</Typography>
+      <Typography className={classes.content}>{survey.content}</Typography>
+
+      <SurveyResult survey={survey} />
+    </Box>
+  );
+
+  const componentBeforeSubmition = (
+    <Box>
+      <Typography className={classes.header}>{survey.title}</Typography>
+      <Typography className={classes.content}>{survey.content}</Typography>
+      <ResponseForm survey={survey} />
+    </Box>
+  );
+
   return (
     <div>
       <Header />
       <Container maxWidth={"md"}>
-        {loading ? (
-          <Loading />
-        ) : (
-          <Box>
-            <Typography className={classes.header}>{survey.title}</Typography>
-            <Typography className={classes.content}>
-              {survey.content}
-            </Typography>
-            <ResponseForm survey={survey} />
-          </Box>
-        )}
+        {hasSubmittedResponse
+          ? componentAfterSubmition
+          : componentBeforeSubmition}
       </Container>
     </div>
   );
