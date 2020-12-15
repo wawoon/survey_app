536de222589ff4e6296e4b60827c3b0a67ea9d26
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-15 10:03:11 +0900

    feature: fetch data from /api/surveys/:id

--- survey_frontend/pages/surveys/[survey_id]/index.tsx
+++ survey_frontend/pages/surveys/[survey_id]/index.tsx
@@ -1,5 +1,47 @@
+import Axios from "axios";
+import { makeStyles } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Header } from "../../../components/Header";
+import store from "../../../store";
+
+const useStyles = makeStyles({
+  header: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
 const SurveyShow = () => {
-  return <div>SurveyShow</div>;
+  const router = useRouter();
+  const [survey, setSurvey] = useState<any>(null);
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (!router.query.survey_id) return;
+
+    const f = async () => {
+      const res = await Axios.get(
+        `http://localhost:3000/api/v1/surveys/${router.query.survey_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${store.getState().auth.accessToken}`,
+          },
+        }
+      );
+      setSurvey(res.data.survey);
+      console.log(res);
+    };
+    f();
+  }, [router.query.survey_id]);
+
+  return (
+    <div>
+      <Header />
+      SurveyShow
+      <div>{JSON.stringify(survey, null, 2)}</div>
+    </div>
+  );
 };
 
 export default SurveyShow;
