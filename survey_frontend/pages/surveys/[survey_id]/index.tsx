4d11e065a906b592dd58b0c912a77ec2aa634eb0
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-15 10:09:34 +0900

    feature: add typing

--- survey_frontend/pages/surveys/[survey_id]/index.tsx
+++ survey_frontend/pages/surveys/[survey_id]/index.tsx
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Header } from "../../../components/Header";
@@ -12,16 +12,40 @@ const useStyles = makeStyles({
   },
 });
 
+type DetailSurvey = {
+  id: number;
+  user_id: number;
+  title: string;
+  content?: string;
+  created_at: string;
+  updated_at: string;
+  questions: {
+    id: number;
+    survey_id: number;
+    name: string;
+    answer_count: number;
+    created_at: string;
+    updated_at: string;
+    choices: {
+      id: number;
+      question_id: number;
+      name: string;
+      created_at: string;
+      updated_at: string;
+    };
+  }[];
+};
+
 const SurveyShow = () => {
   const router = useRouter();
-  const [survey, setSurvey] = useState<any>(null);
+  const [survey, setSurvey] = useState<DetailSurvey | null>(null);
   const classes = useStyles();
 
   useEffect(() => {
     if (!router.query.survey_id) return;
 
     const f = async () => {
-      const res = await Axios.get(
+      const res = await Axios.get<{ survey: DetailSurvey }>(
         `http://localhost:3000/api/v1/surveys/${router.query.survey_id}`,
         {
           headers: {
@@ -38,7 +62,7 @@ const SurveyShow = () => {
   return (
     <div>
       <Header />
-      SurveyShow
+      <Typography>SurveyShow</Typography>
       <div>{JSON.stringify(survey, null, 2)}</div>
     </div>
   );
