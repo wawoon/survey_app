2f163a87ed696db114e350116c084865cee5e025
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2020-12-15 12:38:07 +0900

    feature: save uuid and respondent while creating response

--- survey_backend/app/controllers/api/v1/responses_controller.rb
+++ survey_backend/app/controllers/api/v1/responses_controller.rb
@@ -1,11 +1,26 @@
+require 'uuid'
+
 class Api::V1::ResponsesController < Api::V1::ApplicationController
   def create
-    @response = Response.new
+    if params[:response][:respondent_uuid].present?
+      respondent = Respondent.find_by(response_params[:response][:respondent_uuid])
+    end
+
+    # debugger
+    if respondent.blank?
+      uuid_gen = UUID.new
+      respondent = Respondent.new(uuid: uuid_gen.generate)
+      respondent.save!
+    end
+
+    @response = Response.new(response_params)
+    @response.respondent = respondent
+    # debugger
 
     if @response.save
       render json: { surveys: @response.as_json }
     else
-      render json: { response: @response.as_json, errors: @response.errors }
+      render json: { response: @response.as_json, errors: @response.errors }, status: 400
     end
 
   end
@@ -13,6 +28,6 @@ class Api::V1::ResponsesController < Api::V1::ApplicationController
   private
 
   def response_params
-    params.require(:response).permit(:id, :survey_id, respondent_attributes: [:name, :email], choice_ids: [], )
+    params.require(:response).permit(:id, :survey_id, :user_name, :user_email, choice_ids: [], )
   end
 end
